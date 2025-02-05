77f08a5bcc8f02eb8d182bc4b628d4d50b7bbb30
+ <+>
2025-02-05 16:13:10 -0600

    completed app page, onto next section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import BookList from './components/BookList';
+import SearchBar from './components/SearchBar';
+import AddBookForm from './components/AddBookForm';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    fetch("http://localhost:3000/books")
+    .then(response => response.json())
+    .then(data => setBooks(data));
+  }, []);
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+  const addBook = (newBook) => {
+    fetch("http://localhost:3000/books", {
+      method: "POST",
+      header: {
+        "Content-Type": "Application/Json"
+      },
+      bodu: JSON.stringify(newBook),
+    })
+    .then(response => response.json())
+    .then(data => setBooks([...books, data]));
+  };
+
+  const deleteBook = (id) => {
+    fetch(`http://localhost:3000/books/${id}`, {method: "DELETE"})
+    .then(() => setBooks(books.filter(book => book.id !== id)));
+  };
+
+  const toggleOutOfStock = (id) => {
+    setBooks(books.map(book => book.id === id ? {...books, outOfStock: !book.outOfStock}: book));
+  };
+
+  const filteredBooks = books.filter(book => book.title.toLowerCase().includes(searchQuery.toLowerCase()));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <hi>BookStore</hi>
+      <SearchBar onSearch={handleSearch} />
+      <AddBookForm onAddBook={addBook} />
+      <BookList books={filteredBooks} onDelete={deleteBook} onToggleOutOfStock={toggleOutOfStock} />
     </div>
   );
 }
